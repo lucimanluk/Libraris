702cbb10e1eef1ffd92fdb34222f2b8a7bc327dc
lucimanluk <lucimanluk4000@gmail.com>
2024-12-24 21:51:18 +0200

    Created the navbar and added some components. Still working on it, just the strcture so far.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
+import { FaSearch } from "react-icons/fa";
+
 function Navbar() {
-    return(
-        <>
-        </>
+    return (
+        <div className="flex flex-row justify-between bg-red-900 p-3">
+            <p>Produse</p>
+            <form className="flex flex-row items-center">
+                <input type="search" placeholder="Search for an item" />
+                <FaSearch id="search-icon" />
+            </form>
+            <p>Basket</p>
+        </div>
     )
 }
 
