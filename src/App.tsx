f1a9efc637a44fbf515f9dc47998cb14b2677138
lucimanluk <lucimanluk4000@gmail.com>
2024-12-25 21:06:53 +0200

    Created the pages folder and modified a bit the Navbar aspect.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 function App() {
 
   return (
-    <div className="m-0">
+    <div className="m-0 min-h-screen">
     <Navbar/>     
     </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,28 @@
 import { FaSearch } from "react-icons/fa";
 import { FaBars } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
+import { FaSignInAlt } from "react-icons/fa";
 
 function Navbar() {
     return (
-        <div className="flex flex-row justify-between bg-slate-100 p-3 items-center">
+        <div className="flex flex-row items-center justify-between bg-slate-100 px-3">
             <div className="flex flex-row items-center gap-2 hover:bg-slate-200">
-                <FaBars id="bars"/>
+                <FaBars id="bars" />
                 <p>Products</p>
             </div>
-            <form className="flex flex-row items-center py-1 pl-4 pr-3 bg-slate-200 border rounded-2xl">
-                <input type="search" placeholder="Search for an item" className="bg-inherit focus:outline-none" />
-                <FaSearch id="search-icon" />
-            </form>
-            <div>
-                <FaCartShopping id = "cart"/>
+            <div className="flex flex-row items-center">
+                <form className="flex flex-row items-center py-1 pl-4 pr-3 bg-slate-200 border rounded-2xl">
+                    <input type="search" placeholder="Search for an item" className="bg-inherit focus:outline-none" />
+                    <FaSearch id="search-icon" />
+                </form>
+                <img src = "./src/assets/logo.png" className = "h-[60px] w-[60px]"></img>
+            </div>
+            <div className="flex flex-row items-center items-center gap-3">
+                <div className="flex flex-row items-center gap-3">
+                    <p>Sign in</p>
+                    <FaSignInAlt />
+                </div>
+                <FaCartShopping id="cart" />
             </div>
         </div>
     )
