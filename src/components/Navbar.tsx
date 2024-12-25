acd3a8b18cb5664e4cc6956ab1ce9ffd53cf1310
lucimanluk <lucimanluk4000@gmail.com>
2024-12-25 14:35:13 +0200

    Added the products section of navbar without functionality and also the basket, just logo.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,21 @@
 import { FaSearch } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
 
 function Navbar() {
     return (
-        <div className="flex flex-row justify-between bg-red-900 p-3">
-            <p>Produse</p>
-            <form className="flex flex-row items-center">
-                <input type="search" placeholder="Search for an item" />
+        <div className="flex flex-row justify-between bg-slate-100 p-3 items-center">
+            <div className="flex flex-row items-center gap-2 hover:bg-slate-200">
+                <FaBars id="bars"/>
+                <p>Products</p>
+            </div>
+            <form className="flex flex-row items-center py-1 pl-4 pr-3 bg-slate-200 border rounded-2xl">
+                <input type="search" placeholder="Search for an item" className="bg-inherit focus:outline-none" />
                 <FaSearch id="search-icon" />
             </form>
-            <p>Basket</p>
+            <div>
+                <FaCartShopping id = "cart"/>
+            </div>
         </div>
     )
 }
