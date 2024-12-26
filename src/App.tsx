3b186c426a4e3c695e33a1696fa99058de210405
lucimanluk <lucimanluk4000@gmail.com>
2024-12-26 17:51:54 +0200

    Created the Product component and almost finished styling it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Product from "./components/Product";
 
 function App() {
 
   return (
     <div className="m-0 min-h-screen">
-    <Navbar/>     
+      <Navbar/>    
+      <Product/> 
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,17 +5,17 @@ import { FaSignInAlt } from "react-icons/fa";
 
 function Navbar() {
     return (
-        <div className="flex flex-row items-center justify-between bg-slate-100 px-3">
-            <div className="flex flex-row items-center gap-2 hover:bg-slate-200">
+        <div className="flex flex-row items-center justify-between px-3">
+            <div className="flex flex-row items-center gap-2">
                 <FaBars id="bars" />
                 <p>Products</p>
             </div>
-            <div className="flex flex-row items-center">
-                <form className="flex flex-row items-center py-1 pl-4 pr-3 bg-slate-200 border rounded-2xl">
+            <div className="flex flex-row items-center gap-3">
+                <form className="flex flex-row items-center py-1 pl-4 pr-3 border rounded-2xl">
                     <input type="search" placeholder="Search for an item" className="bg-inherit focus:outline-none" />
                     <FaSearch id="search-icon" />
                 </form>
-                <img src = "./src/assets/logo.png" className = "h-[60px] w-[60px]"></img>
+                <img src = "./src/assets/logo2.png" className = "h-[60px] w-[60px] shadow-md rounded-md"></img>
             </div>
             <div className="flex flex-row items-center items-center gap-3">
                 <div className="flex flex-row items-center gap-3">
